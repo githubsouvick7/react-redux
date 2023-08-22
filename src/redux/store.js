7203867e7b7bf584c6c1b3984b8c660acd1671de
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-08-22 15:22:35 +0530

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+// src/store.js
+import { createStore } from 'redux';
+
+// Initial State
+const initialState = {
+    count: 0,
+};
+
+// Reducer
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { ...state, count: state.count + 1 };
+        case 'DECREMENT':
+            return { ...state, count: state.count - 1 };
+        default:
+            return state;
+    }
+};
+
+// Create Store
+const store = createStore(counterReducer);
+
+export default store;
